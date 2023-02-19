d7b1f30166762a1bbf99a15ffc1dca70ff65ae3f
mperret10 <mperret10@gmail.com>
2023-02-19 16:11:19 -0500

    Update forecast

--- app.js
+++ app.js
@@ -17,34 +17,38 @@ if (minutes < 10) {
 }
 h4.innerHTML = `${day} ${hours}:${minutes}`;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[date.getDay()];
+  return day;
+}
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Mon", "Tues", "Wed", "Thu", "Fri", "Sat", "Sun"];
-
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (forecastDay, index) {
+  forecast.forEach(function (forecastDay, index) {
     if (index < 6) {
       forecastHTML =
         forecastHTML +
         `    <div class="col-2">
             <div class="weather-forecast-date">${formatDay(
-              forecastDay.dt
+              forecastDay.time
             )}</div>
             
-            <img src='http://openweathermap.org/img/wn/${
-              day.weather[0].icon
-            }@2x.png'
+            <img src='http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+              forecastDay.condition.icon
+            }.png'
             alt=""
             width="42" 
             />
             <div class="weather-forecast-temperature">
               <span class="weather-forecast-temperature-max">${Math.round(
-                forecastDay.temp.max
+                forecastDay.temp.maximum
               )}° </span>
               <span class="weather-forecast-temperature-min">${Math.round(
-                forecastDay.temp.min
+                forecastDay.temp.mininimum
               )}° </span>
             </div>
           </div>       
@@ -89,6 +93,7 @@ function showWeather(response) {
   );
 
   iconElement.setAttribute("alt", response.data.condition.description);
+  getForecast(response.data.coordinates);
 }
 
 function search(city) {
@@ -148,5 +153,3 @@ fahrenheitLink.addEventListener("click", showFahrenheitTemperature);
 
 let celciusLink = document.querySelector("#celcius-link");
 celciusLink.addEventListener("click", showCelciusTemperature);
-
-displayForecast();
